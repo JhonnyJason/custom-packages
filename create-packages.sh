b9be9dcbdc7a1d9b053f1ed76251063b76dce79f
Lenard Frommelt <lenny09918050@gmail.com>
2022-03-14 16:25:12 +0100

    remove packages and temporary git repos + copying the built packages instead of linking them

--- create-packages.sh
+++ create-packages.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 mkdir -p ~/builds/custom-packages
+rm ~/repo-packages/custom-packages/*.pkg.tar.xz*
 n=0
 while read gitURL; do 
     git clone $gitURL ~/builds/custom-packages/$n
     pushd ~/builds/custom-packages/$n
     makepkg -s --noconfirm
-    ln -s ~/builds/custom-packages/$n/*.pkg.tar.xz* ~/repo-packages/custom-packages/
+    cp ~/builds/custom-packages/$n/*.pkg.tar.xz* ~/repo-packages/custom-packages/
     popd
     n=$n+1
 done < package-git-urls
+rm -rf ~/builds/custom-packages/*
